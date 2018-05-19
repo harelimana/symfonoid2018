92d8af165599a40a55cd3217cefa5819dcb7a536
harelimana <alexandre.harelimana@gmail.com>
2018-05-19 10:45:52 +0200

    first commit

---
+++ src/Controller/NewsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: axxahretz
+ * Date: 11.05.18
+ * Time: 22:34
+ */
+
+namespace App\Controller;
+
+
+class NewsController
+{
+
+}
---
+++ src/Services/FixturesManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: axxahretz
+ * Date: 11.05.18
+ * Time: 22:23
+ */
+
+namespace App\Services;
+
+
+class FixturesManager
+{
+
+}
